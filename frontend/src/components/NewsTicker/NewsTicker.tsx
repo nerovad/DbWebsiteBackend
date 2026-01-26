80b344a3afbc0fe94cd12aaed6270b159adcbae7
matthewdavoren <matthewdavoren@gmail.com>
2026-01-26 22:33:32 +0000

    font changes from Press Start 2p to Inter

--- frontend/src/components/NewsTicker/NewsTicker.tsx
+++ frontend/src/components/NewsTicker/NewsTicker.tsx
@@ -4,33 +4,49 @@ import "../../styles/_variables.scss";
 
 const NewsTicker: React.FC = () => {
   const tickerRef = useRef<HTMLDivElement>(null);
-  const [duration, setDuration] = useState(50);
+  const [duration, setDuration] = useState(20);
+  const [isMinimized, setIsMinimized] = useState(false);
 
   const tickerText = `Welcome to CineZoo! | Click anywhere on screen to navigate channels | Need to contact us? Email us as cinezoo@gmail.com | Oscars watch along with be on channel 7 this year, March 16th at 4:00 PM PST | Check out channel 99 for Friday Night Rewind: Live!`;
 
   useEffect(() => {
-    if (tickerRef.current) {
+    if (!isMinimized && tickerRef.current) {
       // Get the width of a single instance of the text
       const singleTextWidth = tickerRef.current.scrollWidth / 4; // Divide by 4 since we have 4 copies
       // Speed in pixels per second (adjust this value to change speed)
-      const pixelsPerSecond = 25;
+      const pixelsPerSecond = 50;
       const calculatedDuration = singleTextWidth / pixelsPerSecond;
       setDuration(calculatedDuration);
     }
-  }, []);
+  }, [isMinimized]);
+
+  const toggleMinimize = () => {
+    setIsMinimized(!isMinimized);
+  };
 
   return (
-    <div className="news-ticker">
-      <div
-        className="ticker"
-        ref={tickerRef}
-        style={{ animationDuration: `${duration}s` }}
+    <div className={`news-ticker ${isMinimized ? "minimized" : ""}`}>
+      <button
+        className="ticker-toggle"
+        onClick={toggleMinimize}
+        aria-label={isMinimized ? "Expand news ticker" : "Minimize news ticker"}
       >
-        <span>{tickerText}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-        <span>{tickerText}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-        <span>{tickerText}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-        <span>{tickerText}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-      </div>
+        {isMinimized ? "▲" : "▼"}
+      </button>
+      {!isMinimized && (
+        <div className="ticker-wrapper">
+          <div
+            className="ticker"
+            ref={tickerRef}
+            style={{ animationDuration: `${duration}s` }}
+          >
+            <span>{tickerText}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+            <span>{tickerText}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+            <span>{tickerText}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+            <span>{tickerText}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
